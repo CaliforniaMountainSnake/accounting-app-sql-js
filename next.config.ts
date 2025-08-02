83e1980518cf31b75a3a92d5875385ced23774a0
CaliforniaMountainSnake <CaliforniaMountainSnake1@yandex.ru>
2025-08-02 22:08:55 +0300

    feat: test of github CI/CD.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+const assetPrefix = basePath ? `${basePath}/` : "";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath,
+  assetPrefix,
   webpack: (config, { isServer, webpack }) => {
     console.log(`Webpack version: ${webpack.version}`);
 
